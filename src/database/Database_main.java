31650978f726975c073bb6817998bbcdead594b1
50516021 <akira.musico@gmail.com>
2017-01-30 15:55:20 +0900

    20171023 submit

---
+++ src/database/Database_main.java
@@ -0,0 +1,17 @@
+package database;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import database.Database_view;
+
+public class Database_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Database_view graph = new Database_view();
+        graph.setBounds(5,5,655,455);
+        graph.setVisible(true);
+
+	}
+
+}
